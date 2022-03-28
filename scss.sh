98858fc631e8af7a33c119c87c917e9e108dbb15
cron-ix <github@vrax.de>
2022-03-28 13:36:24 +0200

    option --no-cache added

--- scss.sh
+++ scss.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # scss --sourcemap --watch scss:css-compiled --style compressed
 # scss --update --force --style=compressed scss:css-compiled
-sass --scss --update --force --style=compressed scss:css-compiled
+sass --scss --no-cache --update --force --style=compressed scss:css-compiled
